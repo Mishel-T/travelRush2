e6f7fb2c6c72a47666a903b433ea9f2b8251847b
Mark Pahor <mark@marks-mbp.wdynp.ucr.edu>
2019-09-10 20:21:26 -0700

    rendering all 4 yelp cards

--- travelrush2/client/src/App.js
+++ travelrush2/client/src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from "react";
+import Header from "./components/header/header";
+import Footer from "./components/footer";
 import SearchContainer from "./components/SearchContainer";
 import DropDown from "./components/DropDown";
 import SearchForm from "./components/SearchForm";
 import { InputFlight, InputDrive, InputDate } from "./components/Form";
 import Form from "./components/Form";
 
-import Card from "./components/Card/Weather";
+import WeatherCard from "./components/Card/Weather";
+import Card from "./components/Card/Card"
 
 
 //import images from "./images.json";
 //import "./App.css";
 
-import Header from "./components/header/header";
-import Footer from "./components/footer";
+
 
 class App extends Component {
   state = {};
 
   render() {
-  return [<div />, <Header />, <Footer />, <SearchContainer />, <Card />];
+  return [<div />, <Header />, <Footer />, <SearchContainer />, <WeatherCard />, 
+    <Card name='Restaurants' img="restaurant-img" cardTitle="restaurant-name" value="restaurants" btnName="restaurant" contentID="restaurant-info" title="restaurant-title" rating="restaurant-rating" price="restaurant-price" url="restaurant-url"></Card>,
+    <Card name='Coffee' img="coffee-img" cardTitle="coffee-name" value="coffee" btnName="free_breakfast" contentID="coffee-info" title="coffee-title" rating="coffee-rating" price="coffee-price" url="coffee-url"></Card>,
+    <Card name='Hotels' img="hotel-img" cardTitle="hotel-name" value="hotels" btnName="hotel" contentID="hotel-info" title="hotel-title" rating="hotel-rating" price="hotel-price" url="hotel-url"></Card>];
+
   }
 
 }
