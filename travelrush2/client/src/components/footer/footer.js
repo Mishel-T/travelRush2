5cbd16f20ca52d0518997417e9aa516b43fff4bf
max delzer <emma.delzer@gmail.com>
2019-09-20 13:26:17 -0700

    Removed duplicate backgroundColor attribute

--- travelrush2/client/src/components/footer/footer.js
+++ travelrush2/client/src/components/footer/footer.js
@@ -5,8 +5,7 @@ function Footer() {
     return (
         <div>
             <div style={{display: 'block', padding: '20px', height: '60px', width: '100%',}} />
-            <div style={{backgroundColor: "#F8F8F8",
-    borderTop: "1px solid #E7E7E7",
+            <div style={{borderTop: "1px solid #E7E7E7",
     textAlign: "center",
     padding: "20px",
     position: "fixed",
