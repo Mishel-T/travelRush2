cebe2b2839a8038ef63a867c879a3c2504159f30
max delzer <emma.delzer@gmail.com>
2019-09-21 10:56:18 -0700

    Edited signUp page

--- travelrush2/client/src/components/SignUp/signUp.js
+++ travelrush2/client/src/components/SignUp/signUp.js
@@ -4,16 +4,48 @@
 import React from "react";
 import SignIn from "./signIn.js"
 
+
 function SignIn() {
   return (
     <div>
       <h1>This is a Sign In Page Test</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      <img src={ Logo } className="responsive-img" alt="Logo" />
+            <h2 id="transition-modal-title">Create User Account</h2>
+            <p id="transition-modal-description">
+              Please create account credentials below.
+            </p>
+            <div>
+              <label htmlFor="name">Enter your name: </label>
+              <input type="text" id="name" />
+            </div>
+            <form>
+                  <div className="input-group mb-3">
+                    <div className="input-group-append">
+                      <span className="input-group-text"><i className="fas fa-user" /></span>
+                    </div>
+                    <input type="text" name className="form-control input_user" id="emailInput" placeholder="username" />
+                  </div>
+                  <div className="input-group mb-2">
+                    <div className="input-group-append">
+                      <span className="input-group-text"><i className="fas fa-key" /></span>
+                    </div>
+                    <input type="password" name className="form-control input_pass" id="passwordInput" placeholder="password" />
+                  </div>
+                  <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                      <input type="checkbox" className="custom-control-input" id="customControlInline" />
+                      <label className="custom-control-label" htmlFor="customControlInline">Remember me</label>
+                    </div>
+                  </div>
+                </form>
+                <button type="button" name="button" id="loginButton" className="btn login_btn">Login</button>
+                <div className="d-flex justify-content-center links">
+                  Already have an account? <a href="/login" className="ml-2">Login here!</a>
+                </div>
+                <div className="d-flex justify-content-center links">
+						<a href="#">Forgot your password?</a>
+					</div>
       </p>
     </div>
   );
